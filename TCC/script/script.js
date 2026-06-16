016287b64d412ee791504a7a872d4e12d239b762
João Oliveira <jpjonpersoliva@gmail.com>
2026-06-15 23:33:51 -0300

    Subindo o código feito até o dia 15/06/2026

---
+++ TCC/script/script.js
@@ -0,0 +1,23 @@
+function enviarWhatsapp() {
+    
+        const data = document.getElementById('dataEvento').value;
+    
+        if (!data) {
+            alert("Selecione uma data.");
+            return;
+        }
+    
+        const partes = data.split('-');
+        const dataFormatada = `${partes[2]}/${partes[1]}/${partes[0]}`;
+    
+        const telefone = "5511964016841"; // Número do buffet
+    
+        const mensagem =
+            `Olá! Gostaria de verificar a disponibilidade do buffet para a data ${dataFormatada}. Poderiam me informar os valores e opções de pacotes?`;
+    
+        const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
+    
+        window.open(url, '_blank');
+    }
+
+    
