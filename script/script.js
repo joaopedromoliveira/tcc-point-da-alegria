4e0ce00d62439f7e9a396c9a27d6eb49e59a2017
João Oliveira <jpjonpersoliva@gmail.com>
2026-06-23 17:58:58 -0300

    Melhora na formatação e distribuiçao do FOOTER de todas as telas, Implementação do wpp clicavél fixo no canto esquerdo inferior de todas as páginas.

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ function enviarWhatsapp() {
         const partes = data.split('-');
         const dataFormatada = `${partes[2]}/${partes[1]}/${partes[0]}`;
     
-        const telefone = "5511964016841"; // Número do buffet
+        const telefone = "5511945737905"; // Número do buffet
     
         const mensagem =
             `Olá! Gostaria de verificar a disponibilidade do buffet para a data ${dataFormatada}. Poderiam me informar os valores e opções de pacotes?`;
